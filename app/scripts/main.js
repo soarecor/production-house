d38832dec16bc5636023ac534906ef9db5de5d15
Darren Hebner <darrenwilliamhebner@gmail.com>
2015-12-11 10:19:09 -0500

    updated bower file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,14 @@
-var app = new Vue({
+var Home = Vue.extend({
+    template: '#home'
+});
+
+var Bar = Vue.extend({
+    template: '<p>This is bar!</p>'
+});
+
+var router = new VueRouter();
+
+var App = Vue.extend({
 	el: '#app',
 
 	ready: function(){
@@ -12,38 +22,49 @@ var app = new Vue({
 			})
 		}
 	}
-})
+});
 
-var home = function () {$(".project-modal").hide()};
-var author = function () { console.log("author"); };
-var books = function () { console.log("books"); };
-var viewProject = function (projectId) {
-	var title, 
-		description,
-		imgURL;
-	$(".project-modal").show();
-    console.log("viewBook: bookId is populated: " + projectId);
-    for (var i = 0, n = app.posts.length; i < n; i++){
-    	if (app.posts[i].projectID == projectId){
-    		console.log("Matched! " + app.posts[i].projectName);
-    		title = app.posts[i].projectName;
-    		description = app.posts[i].projectDescr;
-    		imgURL = app.posts[i].imageURL;
-    		$(".project-modal").html("<a href='#/'>back</a>" + "<h1>" + title + "</h1>" + "<p>" + description + "</p>" + "<img src='http://phoenix.sheridanc.on.ca/~shertv/SPH/pImage/" + imgURL + "'>");
-    		break;
-    	}
-    }
-};
+router.map({
+	'/foo': {
+		component: Home
+	},
+	'/bar': {
+		component: Bar
+	}	
+});
 
-var routes = {
-	'/': home,
-    '/author': author,
-    '/books': [books, function() {
-      console.log("An inline route handler.");
-    }],
-    '/project/:projectId': viewProject
-};
+router.start(App, '#app')
 
-var router = Router(routes);
+// var home = function () {$(".project-modal").hide()};
+// var author = function () { console.log("author"); };
+// var books = function () { console.log("books"); };
+// var viewProject = function (projectId) {
+// 	var title, 
+// 		description,
+// 		imgURL;
+// 	$(".project-modal").show();
+//     console.log("viewBook: bookId is populated: " + projectId);
+//     for (var i = 0, n = app.posts.length; i < n; i++){
+//     	if (app.posts[i].projectID == projectId){
+//     		console.log("Matched! " + app.posts[i].projectName);
+//     		title = app.posts[i].projectName;
+//     		description = app.posts[i].projectDescr;
+//     		imgURL = app.posts[i].imageURL;
+//     		$(".project-modal").html("<a href='#/'>back</a>" + "<h1>" + title + "</h1>" + "<p>" + description + "</p>" + "<img src='http://phoenix.sheridanc.on.ca/~shertv/SPH/pImage/" + imgURL + "'>");
+//     		break;
+//     	}
+//     }
+// };
 
-router.init();
+// var routes = {
+// 	'/': home,
+//     '/author': author,
+//     '/books': [books, function() {
+//       console.log("An inline route handler.");
+//     }],
+//     '/project/:projectId': viewProject
+// };
+
+// var router = Router(routes);
+
+// router.init();
